d19a3d9107b9e8ca697e184b594e169594da02f3
Christian Schwarz <me@cschwarz.com>
2017-12-03 16:48:27 +0100

    WIP: libc: protoype API for stagesched

---
+++ include/osv/stagesched.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2017 Christian Schwarz
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+#ifndef _STAGESCHED_H
+#define _STAGESCHED_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void stagesched_set_stage(int stage);
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ libc/stagesched.cc
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2017 Christian Schwarz
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+#include <cstdio>
+
+extern "C" void stagesched_set_stage(int stage)
+{
+    int c = 0;
+    c++;
+}
