9eaf3a8b9aa5a18a5a79920066803a5a84d3b6c5
Christian Schwarz <me@cschwarz.com>
2018-01-09 10:23:42 +0100

    stagesched: provide header-only programming aids for stagifying
    * `sched::stage_stack`
    * `sched::stage_stack::guard``
    * compatibility for the version of C++ used in MySQL 5.6
      (whatever it is, it's < C++11)

--- include/osv/stagesched.h
+++ include/osv/stagesched.h
@@ -9,6 +9,7 @@
 #ifdef __cplusplus
 
 #include <string>
+#include <stack>
 
 namespace sched {
 
@@ -24,9 +25,6 @@ namespace sched {
      */
     class stage {
         public:
-
-            stage(stage const&) = delete;
-            void operator=(stage const&)  = delete;
 
             /**
              * Defines a new stage.
@@ -50,16 +48,92 @@ namespace sched {
 
         private:
             stage() {}
+            stage(stage const&) {}
+            void operator=(stage const&) {}
 
         public:
-            constexpr static int max_stages = 32;
+            static const int max_stages = 32;
 
         private: /* per-instance state */
             int _id;
             std::string _name;
     };
 
-}
-#endif
+    /**
+     * Sometimes, one piece of code P called from multipler callers
+     * should always execute in a stage S.
+     * Thus, it makes sense that P contains the code to migrate to S
+     * to avoid duplication.
+     * But when P is about to return to its caller,
+     * it does not know which stage the caller was in.
+     *
+     * A stack of stages is the natural abstraction for this,
+     * which is what stage_stack is all about.
+     **/
+    class stage_stack {
+        public:
+            stage_stack(stage* default_stage, bool disabled_ = false)
+                : disabled(disabled_) {
+                    begin(default_stage);
+            }
 
+            /**
+             * Begin executing code in a given stage
+             **/
+            void begin(sched::stage* stage) {
+                if (disabled) return;
+                stack.push(stage);
+                stage->enqueue();
+            }
+
+            /**
+             * Finish executing the current stage and return to previous
+             * stage or the default stage specified on initalization.
+             **/
+            void finish() {
+                if (disabled) return;
+                if (stack.size() > 1) {
+                    stack.pop();
+                }
+                stack.top()->enqueue();
+            }
+
+            /**
+             * Switch to a given stage, replacing the current one
+             **/
+            void switch_to(sched::stage* stage) {
+                if (disabled) return;
+                stack.emplace(stage);
+                stage->enqueue();
+            }
+
+        private:
+            std::stack<sched::stage*> stack;
+            bool disabled;
+
+        public:
+
+            /**
+             * A guard is the equivalent of a std::lock_guard for stage_stack.
+             * It uses the RAII paradigm to push the next stage onto the
+             * stage_stack on initialization and pops from that stack at
+             * the en* of scope.
+             **/
+            class guard {
+                public:
+                    guard(stage_stack& stack, stage *next) : s(stack) {
+                        s.begin(next);
+                    }
+                    ~guard() {
+                        s.finish();
+                    }
+                private:
+                    stage_stack& s;
+            };
+
+    };
+
+}
+
+#endif /* __cplusplus */
 #endif /*_STAGESCHED_H */
