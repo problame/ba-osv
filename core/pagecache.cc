da3cde15e0e7eb571a526b3d8570bbe58adb7a5b
Christian Schwarz <me@cschwarz.com>
2018-02-21 16:10:58 +0100

    change pageaccess scanner ot 10HZ

--- core/pagecache.cc
+++ core/pagecache.cc
@@ -557,7 +557,7 @@ TRACEPOINT(trace_access_scanner, "scanned=%u, cleared=%u, %%cpu=%g", unsigned, u
 static class access_scanner {
     static constexpr double _max_cpu = 20;
     static constexpr double _min_cpu = 0.1;
-    static constexpr unsigned _freq = 1000;
+    static constexpr unsigned _freq = 10;
     double _cpu = _min_cpu;
     std::unique_ptr<sched::thread> _thread;
 public:
